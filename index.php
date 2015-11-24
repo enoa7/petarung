a80c1a3b4e0b5950a04216c53ebba55ea796a6e2
enoa7 <gamal.ardi@gmail.com>
2015-11-24 23:45:45 +0700

    main navigation completed

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 	include("header.php");
 	$buffer=ob_get_contents();
 	ob_end_clean();
-	$buffer=str_replace("%TITLE%","Petarung Indonesia || Beranda ",$buffer);
+	$buffer=str_replace("%TITLE%","Petarung Indonesia | Beranda ",$buffer);
 	echo $buffer;
 ?>
 
 <?php require('nav.php') ?>
-
+<div class="red_box"></div>
 <div class="page-wrap ulasan">
     <div class="page-banner">
         <div class="container">
@@ -18,5 +18,13 @@
         </div>
     </div>
 </div>
+
+<style type="text/css">
+    .red_box {
+        width: 100%;
+        height: 100%;
+        background: red;
+    }
+</style>
 
 <?php require('footer.php') ?>
--- nav.php
+++ nav.php
@@ -6,14 +6,23 @@
             <div class="logo col-xs-4 nopaddingleft">
                 <img class="img-responsive"src="asset/images/petarung_logo_mobile.png">
             </div>
-            <div class="header_form col-xs-7 pull-right nopaddingright">
-                <div class="col-xs-4 text-left pull-right hamburger">
+            <div class="header_form col-xs-7 nopaddingright">
+                <div class="hamburger">
                     <i class="fa fa-bars hover"></i>
                 </div>
-                <div class="col-xs-2 separator pull-right text-center">|</div>
-                <div class="col-xs-4 text-right pull-right searchbox">
+                <div class="login">log in</div>
+                <div class="searchbox">
                     <i class="fa fa-search hover"></i>
                 </div>
+                <!-- <div class="col-xs-3 text-left pull-right hamburger">
+                    <i class="fa fa-bars hover"></i>
+                </div>
+                <div class="col-xs-1 separator pull-right text-center">|</div>
+                <div class="col-xs-4 pull-right text-center">log in</div>
+                <div class="col-xs-1 separator pull-right text-center">|</div>
+                <div class="col-xs-3 text-right pull-right searchbox">
+                    <i class="fa fa-search hover"></i>
+                </div> -->
                 <!-- <form class="">
                     <div class="form-group col-xs-12 searchbox">
                         <input type="text" placeholder="Cari topik, artikel, atau ulasan" class="form-control">
@@ -36,7 +45,7 @@
         <div class="logo col-sm-4 col-md-7">
             <img class="img-responsive"src="asset/images/petarung_logo.jpg">
         </div>
-        <div class="header_form col-sm-8 col-md-5">
+        <div class="header_form col-sm-8 col-md-5 nopaddingright">
             <form class="">
                 <div class="form-group col-xs-12 searchbox">
                     <input type="text" placeholder="Cari topik, artikel, atau ulasan" class="form-control">
@@ -52,6 +61,46 @@
             <div class="ad-text col-xs-12 visible-md visible-lg">
             <div><span class="highlight">LOG IN</span> atau <span class="highlight">DAFTAR SEKARANG</span> untuk sesuaikan berita pilihan Anda</div>
                 
+            </div>
+        </div>
+        <div class="header_nav col-xs-12 spacepad-nosides">
+            <div class="container">
+                <div class="menu-list col-sm-8 nopadding">
+                    <ul class="nodots nopaddingleft nopaddingright nomarginbottom clearfix">
+                        <li class="left"><a href=""><span>Beranda</span></a></li>
+                        <li class="left"><a href=""><span>Berita Bela Diri</span></a></li>
+                        <li class="left"><a href=""><span>Artikel</span></a></li>
+                        <li class="left"><a href=""><span>Ulasan</span></a></li>
+                        <li class="left"><a href=""><span>Kontak</span></a></li>
+                    </ul>
+                </div>
+                <div class="social-list col-sm-4">
+                    <div>
+                        <svg class="social-icons">
+                          <use xlink:href="asset/images/icons/social_icons.svg#rss"></use>
+                        </svg>
+                    </div>
+                    <div>
+                        <svg class="social-icons">
+                          <use xlink:href="asset/images/icons/social_icons.svg#instagram"></use>
+                        </svg>
+                    </div>
+                    <div>
+                        <svg class="social-icons">
+                          <use xlink:href="asset/images/icons/social_icons.svg#google_plus"></use>
+                        </svg>
+                    </div>
+                    <div>
+                        <svg class="social-icons">
+                          <use xlink:href="asset/images/icons/social_icons.svg#fb"></use>
+                        </svg>
+                    </div>
+                    <div>
+                        <svg class="social-icons">
+                          <use xlink:href="asset/images/icons/social_icons.svg#twitter"></use>
+                        </svg>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
