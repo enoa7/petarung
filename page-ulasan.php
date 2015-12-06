8b3753a41fac3aa30bf71333395b5201f5a02745
enoa7 <gamal.ardi@gmail.com>
2015-12-06 20:12:46 +0700

    template, admin edit
    # edit admin index and home
    # edit detailberita.php for used as a template (currently using
    page-ulasan as a ref)

--- page-ulasan.php
+++ page-ulasan.php
@@ -31,23 +31,23 @@
                     <div class="item-category">Perlengkapan MMA</div>
                     <div class="item-name">Consectur MMA Gloves</div>
                     <div class="item-status-list clearfix spacepad">
-                        <div class="item-status design" data-value=".75" data-fcolor="#DC583B" data-bcolor="#F7A08B">
+                        <div class="item-status design" data-value="0.75" data-fcolor="#DC583B" data-bcolor="#F7A08B">
                             <span class="stat-value"></span>
                             <div class="stat-attr">Design</div>
                         </div>
-                        <div class="item-status usability" data-value=".55" data-fcolor="#F7A231" data-bcolor="#F8C989">
+                        <div class="item-status usability" data-value="0.55" data-fcolor="#F7A231" data-bcolor="#F8C989">
                             <span class="stat-value"></span>
                             <div class="stat-attr">Usability</div>
                         </div>
-                        <div class="item-status detail" data-value=".81" data-fcolor="#E7643D" data-bcolor="#F69A8C">
+                        <div class="item-status detail" data-value="0.81" data-fcolor="#E7643D" data-bcolor="#F69A8C">
                             <span class="stat-value"></span>
                             <div class="stat-attr">Detail</div>
                         </div>
-                        <div class="item-status material" data-value=".92" data-fcolor="#972F88" data-bcolor="#B670AE">
+                        <div class="item-status material" data-value="0.92" data-fcolor="#972F88" data-bcolor="#B670AE">
                             <span class="stat-value"></span>
                             <div class="stat-attr">Material</div>
                         </div>
-                        <div class="item-status price" data-value=".67" data-fcolor="#42A99F" data-bcolor="#99CCC4">
+                        <div class="item-status price" data-value="0.67" data-fcolor="#42A99F" data-bcolor="#99CCC4">
                             <span class="stat-value"></span>
                             <div class="stat-attr">Price</div>
                         </div>
@@ -55,6 +55,7 @@
                             <span class="stat-value"></span>
                         </div> -->
                     </div>
+                    <div class="average"></div>
                     <div class="item-post clearfix">
                         <div class="item-post-date left">
                             <div class="item-post-date-day">jumat</div>
@@ -69,6 +70,36 @@
                         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
 
                         <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="comment mobile hide">
+                <div class="container-fluid">
+                    <form action="" method="POST" role="form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="" placeholder="Tulis komentar ...">
+                        </div>
+                        <div class="text-right"><button type="submit" class="btn btn-primary">Submit</button></div>
+                    </form>
+                </div>
+            </div>
+            <div class="related-product">
+                <div>
+                    <div class="col-xs-6 nopadding">
+                        <div class="img"><img class="img-responsive" src="asset/images/produck-terkait1.jpg"></div>
+                        <div class="text">test</div>
+                    </div>
+                    <div class="col-xs-6 nopadding">
+                        <div class="img"><img class="img-responsive" src="asset/images/produck-terkait2.jpg"></div>
+                        <div class="text">test</div>
+                    </div>
+                    <div class="col-xs-6 nopadding">
+                        <div class="img"><img class="img-responsive" src="asset/images/produck-terkait3.jpg"></div>
+                        <div class="text">test</div>
+                    </div>
+                    <div class="col-xs-6 nopadding">
+                        <div class="img"><img class="img-responsive" src="asset/images/produck-terkait4.jpg"></div>
+                        <div class="text">test</div>
                     </div>
                 </div>
             </div>
